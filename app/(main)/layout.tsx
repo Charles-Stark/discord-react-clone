5f3506e6aff7e307a372f7a694caae2ad59060c6
charles_stark <1614869409@qq.com>
2023-12-11 21:40:51 +0800

    add navigation sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 
 export default async function MainLayout({
   children,
@@ -7,7 +8,9 @@ export default async function MainLayout({
 }) {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"></div>
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
       <div className="md:pl-[72px] h-full">{children}</div>
     </div>
   );
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,5 @@
+export default function NavigationSidebar() {
+  return (
+    <div>Navigation Sidebar</div>
+  )
+}
