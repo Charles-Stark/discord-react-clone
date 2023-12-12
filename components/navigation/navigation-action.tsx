79d41e7b4a5dee231616a6c552039797401adf57
charles_stark <1614869409@qq.com>
2023-12-12 15:00:48 +0800

    add button action and modify color

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,17 +2,22 @@
 
 import { Plus } from "lucide-react";
 import ActionTooltip from "@/components/astion-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 export default function NavigationAction() {
+  const { onOpen } = useModal();
+
   return (
     <div>
-      <ActionTooltip
-        side="right"
-        align="center"
-        label="Add a server"
-      >
-        <button className="group flex items-center">
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-500 group-hover:bg-emerald-500">
+      <ActionTooltip side="right" align="center" label="Add a server">
+        <button
+          onClick={() => {
+            console.log("clicked");
+            onOpen("createServer");
+          }}
+          className="group flex items-center"
+        >
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-[#2B2D31] group-hover:bg-emerald-500">
             <Plus
               className="group-hover:text-white transition text-emerald-500"
               size={25}
