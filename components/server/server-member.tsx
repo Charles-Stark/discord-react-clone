cb86f351d2a0931d8a278653a8e07b563730d7e5
charles_stark <1614869409@qq.com>
2023-12-14 13:28:28 +0800

    add member and channel page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -23,7 +23,7 @@ export default function ServerMember({ member, server }: ServerMemberProps) {
   const params = useParams();
   const router = useRouter();
 
-  const icon = roleIconMap[member.role]
+  const icon = roleIconMap[member.role];
 
   const onClick = () => {
     router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
@@ -31,7 +31,7 @@ export default function ServerMember({ member, server }: ServerMemberProps) {
 
   return (
     <button
-      // onClick={onClick}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700",
