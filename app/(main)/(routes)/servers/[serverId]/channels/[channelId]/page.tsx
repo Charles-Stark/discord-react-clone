7962946073b17e7933359fb5bcffad8b40340191
charles_stark <1614869409@qq.com>
2023-12-15 16:40:37 +0800

    add chat-input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import ChatHeader from "@/components/chat/chat-header";
 import { ChannelType } from "@prisma/client";
+import ChatInput from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -43,7 +44,20 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
         serverId={channel.serverId}
         type="channel"
       />
-      {channel.type === ChannelType.TEXT && <>Chat message</>}
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <div className="flex-1">Chat message</div>
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
       {channel.type === ChannelType.AUDIO && <>MediaRoom - audio</>}
       {channel.type === ChannelType.VIDEO && <>MediaRoom - video</>}
     </div>
