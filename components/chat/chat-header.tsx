7a0c5718745ea6fb338b93bdf5c9728c1fc5a7f5
charles_stark <1614869409@qq.com>
2023-12-15 16:10:27 +0800

    update

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import { UserAvatar } from "@/components/user-avatar";
 import MobileToggle from "@/components/mobile-toggle";
+import SocketIndicator from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -27,7 +28,7 @@ export default function ChatHeader({
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
         {type === "conversation" && "chatVideoButton"}
-        socketIndicator
+        <SocketIndicator />
       </div>
     </div>
   );
