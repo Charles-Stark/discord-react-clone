641f8f5887d11e87f983b5545d9f221189a3e39c
charles_stark <1614869409@qq.com>
2023-12-16 16:19:52 +0800

    update

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,6 +2,9 @@
 
 import { Member } from "@prisma/client";
 import ChatWelcome from "@/components/chat/chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { ElementRef, useRef } from "react";
+import { Loader2, ServerCrash } from "lucide-react";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -28,13 +31,48 @@ export default function ChatMessages({
   paramValue,
   type,
 }: ChatMessagesProps) {
-  return <div
-    className="flex-1 flex flex-col py-4 overflow-y-auto"
-  >
-    <div className="flex-1" />
-    <ChatWelcome
-      type={type}
-      name={name}
-    />
-  </div>;
+  const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
+
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    });
+  // useChatSocket({ queryKey, addKey, updateKey });
+
+  if (status === "loading") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
+    </div>
+  );
 }
